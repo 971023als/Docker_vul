b95fc8acac50571095ed4fde6db0cae8bdcc7fcc
971023als <971023als@naver.com>
2024-04-14 01:10:42 +0900

    Update 1.3.sh

--- 1.3.sh
+++ 1.3.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-# 디렉터리 설정
-output_dir="./aws_iam_tags_audit"
-mkdir -p $output_dir
+# Variables initialization
+분류="Docker Configuration"
+코드="1.3"
+위험도="중요도 상"
+진단_항목="SSL/TLS 적용"
+대응방안="{
+  \"설명\": \"TLS(Transport Layer Security)는 정보를 암호화해서 송수신하는 프로토콜로, Docker가 TCP Socket을 통해 네트워크에 연결되는 경우 SSL/TLS를 적용하여 데이터와 사용자 인증을 보호합니다. 주기적인 인증서 갱신과 안전한 암호화 방식 사용이 필수입니다.\",
+  \"설정방법\": [
+    \"OpenSSL을 사용하여 CA, 서버 및 클라이언트 키 생성\",
+    \"Docker 서비스 실행 시 SSL/TLS 옵션 적용: docker --tlsverify --tlscacert=ca.pem --tlscert=cert.pem --tlskey=key.pem -H=$HOST:2376 version\"
+  ]
+}"
+현황=()
+진단_결과=""
 
-# IAM 사용자 태그 조회 및 진단
-echo "Fetching IAM Users and evaluating tags..."
-aws iam list-users --output json > $output_dir/users.json
+# 진단 시작
+echo "진단 시작..."
+# Check Docker daemon for SSL/TLS configuration
+echo "검사: Docker daemon SSL/TLS 설정 확인"
+ps -ef | grep dockerd | grep -- "--tlsverify"
 
-# 태그 평가 및 결과 저장
-echo "[]" > $output_dir/tag_audit_results.json  # 초기 JSON 배열 파일 생성
+# 임시 진단 결과 할당
+진단_결과="양호" # 또는 "취약"을 할당할 수 있습니다. 검사 후 결정
 
-jq -r '.Users[] | .UserName' $output_dir/users.json | while read user; do
-  user_tags=$(aws iam list-user-tags --user-name "$user" --output json)
-  name_tag=$(echo $user_tags | jq -r '.Tags[] | select(.Key == "Name") | .Value')
-  email_tag=$(echo $user_tags | jq -r '.Tags[] | select(.Key == "Email") | .Value')
-  department_tag=$(echo $user_tags | jq -r '.Tags[] | select(.Key == "Department") | .Value')
-
-  # 결과 생성 및 저장
-  jq -n --arg user "$user" --arg name_tag "$name_tag" --arg email_tag "$email_tag" --arg department_tag "$department_tag" \
-  '{"user": $user, "Name": $name_tag, "Email": $email_tag, "Department": $department_tag}' >> $output_dir/tag_audit_results.json
-done
-
-echo "Audit complete. Results saved in $output_dir."
+# 결과 JSON 출력
+echo "{
+  \"분류\": \"$분류\",
+  \"코드\": \"$코드\",
+  \"위험도\": \"$위험도\",
+  \"진단_항목\": \"$진단_항목\",
+  \"대응방안\": $대응방안,
+  \"현황\": $현황,
+  \"진단_결과\": \"$진단_결과\"
+}"
